f4c397a953df41b778c6012f3024747672d84be8
Barbier--Darnal Joseph <joseph.barbierdarnal@gmail.com>
2025-10-12 10:15:56 +0200

    add raw function for Quarto documents

---
+++ R/raw.R
@@ -0,0 +1,21 @@
+#' @title Raw blocks for Rmarkdown/Quarto
+#'
+#' @description
+#' Convert to raw Typst for Rmarkdown and Quarto.
+#' Using this function is required to include the
+#' output of `{rtyp}` in your document.
+#'
+#' This is a simple wrapper around [`knitr::raw_block()`].
+#'
+#' @param x A character vector to interpret as raw typst.
+#'
+#' @returns A raw version, wrapped in the expected format
+#' for Rmarkdown and Quarto document.
+#'
+#' @examples
+#' heading(level = 2, numbering = "1.1", "Hello world") |> as_raw_typ()
+#'
+#' typ_markup <- heading(level = 2, numbering = "1.1", "Hello world")
+#' as_raw_typ(typ_markup)
+#' @export
+as_raw_typ <- function(x) knitr::raw_block(x, "typst")
