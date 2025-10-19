1b7491efad844372f9e0d7bc8a7e907b386afa85
Barbier--Darnal Joseph <joseph.barbierdarnal@gmail.com>
2025-10-19 20:26:29 +0200

    add tests for #14

--- tests/testthat/test-text.R
+++ tests/testthat/test-text.R
@@ -1,9 +1,11 @@
 test_that("Test text functions", {
   out <- text(font = "Libertinus Serif", "Hello world")
   expect_equal(out, "#text(font: \"Libertinus Serif\")[Hello world]")
+  expect_true(out |> is_valid_typst())
 
   out <- text("Just text")
   expect_equal(out, "#text[Just text]")
+  expect_true(out |> is_valid_typst())
 
   out <- text(
     size = pt(12),
@@ -21,33 +23,43 @@ test_that("Test text functions", {
 
   out <- linebreak()
   expect_equal(out, "#linebreak()")
+  expect_true(out |> is_valid_typst())
 
   out <- linebreak(justify = TRUE)
   expect_equal(out, "#linebreak(justify: true)")
+  expect_true(out |> is_valid_typst())
 
   out <- highlight(`top-edge` = "ascender", "hey")
   expect_equal(out, "#highlight(top-edge: \"ascender\")[hey]")
+  expect_true(out |> is_valid_typst())
 
   out <- lower("hey")
   expect_equal(out, "#lower[hey]")
+  expect_true(out |> is_valid_typst())
 
   out <- upper("you")
   expect_equal(out, "#upper[you]")
+  expect_true(out |> is_valid_typst())
 
   out <- overline(offset = em(-1.2), "ice cream")
   expect_equal(out, "#overline(offset: -1.2em)[ice cream]")
+  expect_true(out |> is_valid_typst())
 
-  out <- overline(offset = fr(-1.2), "ice cream")
-  expect_equal(out, "#overline(offset: -1.2fr)[ice cream]")
+  out <- overline(offset = em(-1.2), "ice cream")
+  expect_equal(out, "#overline(offset: -1.2em)[ice cream]")
+  expect_true(out |> is_valid_typst())
 
   out <- smallcaps(all = TRUE, "UNICEF")
   expect_equal(out, "#smallcaps(all: true)[UNICEF]")
+  expect_true(out |> is_valid_typst())
 
-  out <- smartquote("smart", double = FALSE)
-  expect_equal(out, "#smartquote(double: false)[smart]")
+  out <- smartquote(double = FALSE)
+  expect_equal(out, "#smartquote(double: false)")
+  expect_true(out |> is_valid_typst())
 
-  out <- strike("smart", double = FALSE)
-  expect_equal(out, "#strike(double: false)[smart]")
+  out <- strike("smart", background = TRUE)
+  expect_equal(out, "#strike(background: true)[smart]")
+  expect_true(out |> is_valid_typst())
 
   out <- sub_("smart", double = FALSE)
   expect_equal(out, "#sub(double: false)[smart]")
