f1316307dd38f6cf15abb0ac7383a3aaca061f38
Barbier--Darnal Joseph <joseph.barbierdarnal@gmail.com>
2025-10-28 16:40:53 +0100

    add all layout set rules #22

--- R/functions_layout.R
+++ R/functions_layout.R
@@ -4,11 +4,23 @@
 #' place(dx = pt(50), "pasta")
 place <- function(...) typst_function("place", ...)
 
+#' @rdname typst_set
+#' @export
+#' @examples
+#' set_place(dx = pt(50))
+set_place <- function(...) typst_set("place", ...)
+
 #' @rdname typst_function
 #' @export
 #' @examples
 #' align(center + horizon, "hey")
 align <- function(...) typst_function("align", ...)
+
+#' @rdname typst_set
+#' @export
+#' @examples
+#' set_align(center + horizon)
+set_align <- function(...) typst_set("align", ...)
 
 #' @rdname typst_function
 #' @export
@@ -16,11 +28,23 @@ align <- function(...) typst_function("align", ...)
 #' h(fr(3))
 h <- function(...) typst_function("h", ...)
 
+#' @rdname typst_set
+#' @export
+#' @examples
+#' set_h(weak = TRUE)
+set_h <- function(...) typst_set("h", ...)
+
 #' @rdname typst_function
 #' @export
 #' @examples
 #' v(percent(3), weak = TRUE)
 v <- function(...) typst_function("v", ...)
+
+#' @rdname typst_set
+#' @export
+#' @examples
+#' set_v(weak = TRUE)
+set_v <- function(...) typst_set("v", ...)
 
 #' @rdname typst_function
 #' @export
@@ -28,11 +52,23 @@ v <- function(...) typst_function("v", ...)
 #' block(breakable = FALSE, width = percent(50), height = "auto", fill = aqua, "cookies")
 block <- function(...) typst_function("block", ...)
 
+#' @rdname typst_set
+#' @export
+#' @examples
+#' set_block(breakable = FALSE, width = percent(50), height = "auto", fill = aqua)
+set_block <- function(...) typst_set("block", ...)
+
 #' @rdname typst_function
 #' @export
 #' @examples
 #' box(clip = FALSE, width = percent(50), height = "auto", fill = red, "ice cream")
 box <- function(...) typst_function("box", ...)
+
+#' @rdname typst_set
+#' @export
+#' @examples
+#' set_box(clip = FALSE, width = percent(50), height = "auto", fill = red)
+set_box <- function(...) typst_set("box", ...)
 
 #' @rdname typst_function
 #' @export
@@ -40,11 +76,23 @@ box <- function(...) typst_function("box", ...)
 #' colbreak(weak = TRUE)
 colbreak <- function(...) typst_function("colbreak", ...)
 
+#' @rdname typst_set
+#' @export
+#' @examples
+#' set_colbreak(weak = TRUE)
+set_colbreak <- function(...) typst_set("colbreak", ...)
+
 #' @rdname typst_function
 #' @export
 #' @examples
 #' stack(dir = ttb, "hey", "you", "!")
 stack <- function(...) typst_function("stack", ...)
+
+#' @rdname typst_set
+#' @export
+#' @examples
+#' set_stack(dir = ttb)
+set_stack <- function(...) typst_set("stack", ...)
 
 #' @rdname typst_function
 #' @export
@@ -52,11 +100,21 @@ stack <- function(...) typst_function("stack", ...)
 #' grid(columns = 2, rows = 2, gutter = pt(3), "hey", "cookies", "ice", "cream")
 grid <- function(...) typst_function("grid", ...)
 
+#' @rdname typst_set
+#' @export
+#' @examples
+#' set_grid(columns = 2, rows = 2, gutter = pt(3))
+set_grid <- function(...) typst_set("grid", ...)
+
 #' @rdname typst_function
 #' @export
 #' @examples
 #' hide("this is hidden")
 hide <- function(...) typst_function("hide", ...)
+
+#' @rdname typst_set
+#' @export
+set_hide <- function(...) typst_set("hide", ...)
 
 #' @rdname typst_function
 #' @export
@@ -64,17 +122,35 @@ hide <- function(...) typst_function("hide", ...)
 #' pagebreak("this is hidden")
 pagebreak <- function(...) typst_function("pagebreak", ...)
 
+#' @rdname typst_set
+#' @export
+#' @examples
+#' set_pagebreak(weak = TRUE)
+set_pagebreak <- function(...) typst_set("pagebreak", ...)
+
 #' @rdname typst_function
 #' @export
 #' @examples
 #' move(dx = pt(3), dy = pt(5), "pasta")
 move <- function(...) typst_function("move", ...)
 
+#' @rdname typst_set
+#' @export
+#' @examples
+#' set_move(dx = pt(3), dy = pt(5))
+set_move <- function(...) typst_set("move", ...)
+
 #' @rdname typst_function
 #' @export
 #' @examples
 #' pad(x = percent(0) + pt(5), y = percent(10) + pt(0), image("file.svg"))
 pad <- function(...) typst_function("pad", ...)
+
+#' @rdname typst_set
+#' @export
+#' @examples
+#' set_pad(x = percent(0) + pt(5), y = percent(10) + pt(0))
+set_pad <- function(...) typst_set("pad", ...)
 
 #' @rdname typst_function
 #' @export
@@ -91,14 +167,36 @@ pad <- function(...) typst_function("pad", ...)
 #')
 page <- function(...) typst_function("page", ...)
 
+#' @rdname typst_set
+#' @export
+#' @examples
+#' set_page(
+#'  flipped = TRUE,
+#'  columns = 2,
+#'  fill = red
+#')
+set_page <- function(...) typst_set("page", ...)
+
 #' @rdname typst_function
 #' @export
 #' @examples
 #' scale(x = percent(-100), "This is mirrored")
 scale <- function(...) typst_function("scale", ...)
 
+#' @rdname typst_set
+#' @export
+#' @examples
+#' set_scale(x = percent(-100))
+set_scale <- function(...) typst_set("scale", ...)
+
 #' @rdname typst_function
 #' @export
 #' @examples
 #' skew(ax = deg(-12), "This is some fake italic text.")
 skew <- function(...) typst_function("skew", ...)
+
+#' @rdname typst_set
+#' @export
+#' @examples
+#' set_skew(ax = deg(-12))
+set_skew <- function(...) typst_set("skew", ...)
--- tests/testthat/test-foundations.R
+++ tests/testthat/test-foundations.R
@@ -1,74 +1,67 @@
 test_that("Test foundations functions", {
-  out <- text(`stylistic-set` = c(1, 2, 3), "10 years ago")
-  expect_equal(out, "#text(stylistic-set: (1, 2, 3))[10 years ago]")
-  expect_true(out |> is_valid_typst())
-
-  out <- assert(1 < 2, message = "math broke")
-  expect_equal(out, "#assert(message: \"math broke\", TRUE)")
-
   out <- bytes(c(1, 2, 3))
   expect_equal(out, "#bytes((1, 2, 3))")
-  expect_true(out |> is_valid_typst())
+  expect_true(out |> is_valid_typst(error_on_failure = TRUE))
 
   out <- bytes(list(1, 2, 3))
   expect_equal(out, "#bytes((1, 2, 3))")
-  expect_true(out |> is_valid_typst())
+  expect_true(out |> is_valid_typst(error_on_failure = TRUE))
 
   out <- bytes("hey")
   expect_equal(out, "#bytes(\"hey\")")
-  expect_true(out |> is_valid_typst())
+  expect_true(out |> is_valid_typst(error_on_failure = TRUE))
 
   out <- datetime(year = 2012, month = 8, day = 3)
   expect_equal(out, "#datetime(year: 2012, month: 8, day: 3)")
-  expect_true(out |> is_valid_typst())
+  expect_true(out |> is_valid_typst(error_on_failure = TRUE))
 
   out <- text(
     costs = list(hyphenation = percent(100), runt = percent(100)),
     "hey"
   )
   expect_equal(out, "#text(costs: (hyphenation: 100%, runt: 100%))[hey]")
-  expect_true(out |> is_valid_typst())
+  expect_true(out |> is_valid_typst(error_on_failure = TRUE))
 
   out <- duration(days = 3, hours = 12)
   expect_equal(out, "#duration(days: 3, hours: 12)")
-  expect_true(out |> is_valid_typst())
+  expect_true(out |> is_valid_typst(error_on_failure = TRUE))
 
   out <- eval_("*Markup!*", mode = "markup")
   expect_equal(out, "#eval(mode: \"markup\", \"*Markup!*\")")
-  expect_true(out |> is_valid_typst())
+  expect_true(out |> is_valid_typst(error_on_failure = TRUE))
 
   out <- panic("this is wrong")
   expect_equal(out, "#panic(\"this is wrong\")")
 
   out <- regex("\\d+")
   expect_equal(out, "#regex(\"\\d+\")")
-  expect_true(out |> is_valid_typst())
+  expect_true(out |> is_valid_typst(error_on_failure = TRUE))
 
   out <- repr("hello")
   expect_equal(out, "#repr(\"hello\")")
-  expect_true(out |> is_valid_typst())
+  expect_true(out |> is_valid_typst(error_on_failure = TRUE))
 
   out <- str_(10)
   expect_equal(out, "#str(10)")
-  expect_true(out |> is_valid_typst())
+  expect_true(out |> is_valid_typst(error_on_failure = TRUE))
 
   out <- symbol("a")
   expect_equal(out, "#symbol(\"a\")")
-  expect_true(out |> is_valid_typst())
+  expect_true(out |> is_valid_typst(error_on_failure = TRUE))
 
   out <- decimal("10")
   expect_equal(out, "#decimal(\"10\")")
-  expect_true(out |> is_valid_typst())
+  expect_true(out |> is_valid_typst(error_on_failure = TRUE))
 
   out <- type(14.7)
   expect_equal(out, "#type(14.7)")
-  expect_true(out |> is_valid_typst())
+  expect_true(out |> is_valid_typst(error_on_failure = TRUE))
 
   out <- version(1, 2, 3, 4)
   expect_equal(out, "#version(1, 2, 3, 4)")
-  expect_true(out |> is_valid_typst())
+  expect_true(out |> is_valid_typst(error_on_failure = TRUE))
 
   out <- version(c(1, 2, 3, 4))
   expect_equal(out, "#version((1, 2, 3, 4))")
-  expect_true(out |> is_valid_typst())
+  expect_true(out |> is_valid_typst(error_on_failure = TRUE))
 })
