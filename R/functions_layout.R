0770a0baa5e3c48e7e8abd918597768655c6372a
Barbier--Darnal Joseph <joseph.barbierdarnal@gmail.com>
2025-10-28 16:04:42 +0100

    add all models set rules #22

--- R/functions_layout.R
+++ R/functions_layout.R
@@ -1,82 +1,82 @@
-#' @rdname typst_functions
+#' @rdname typst_function
 #' @export
 #' @examples
 #' place(dx = pt(50), "pasta")
 place <- function(...) typst_function("place", ...)
 
-#' @rdname typst_functions
+#' @rdname typst_function
 #' @export
 #' @examples
 #' align(center + horizon, "hey")
 align <- function(...) typst_function("align", ...)
 
-#' @rdname typst_functions
+#' @rdname typst_function
 #' @export
 #' @examples
 #' h(fr(3))
 h <- function(...) typst_function("h", ...)
 
-#' @rdname typst_functions
+#' @rdname typst_function
 #' @export
 #' @examples
 #' v(percent(3), weak = TRUE)
 v <- function(...) typst_function("v", ...)
 
-#' @rdname typst_functions
+#' @rdname typst_function
 #' @export
 #' @examples
 #' block(breakable = FALSE, width = percent(50), height = "auto", fill = aqua, "cookies")
 block <- function(...) typst_function("block", ...)
 
-#' @rdname typst_functions
+#' @rdname typst_function
 #' @export
 #' @examples
 #' box(clip = FALSE, width = percent(50), height = "auto", fill = red, "ice cream")
 box <- function(...) typst_function("box", ...)
 
-#' @rdname typst_functions
+#' @rdname typst_function
 #' @export
 #' @examples
 #' colbreak(weak = TRUE)
 colbreak <- function(...) typst_function("colbreak", ...)
 
-#' @rdname typst_functions
+#' @rdname typst_function
 #' @export
 #' @examples
 #' stack(dir = ttb, "hey", "you", "!")
 stack <- function(...) typst_function("stack", ...)
 
-#' @rdname typst_functions
+#' @rdname typst_function
 #' @export
 #' @examples
 #' grid(columns = 2, rows = 2, gutter = pt(3), "hey", "cookies", "ice", "cream")
 grid <- function(...) typst_function("grid", ...)
 
-#' @rdname typst_functions
+#' @rdname typst_function
 #' @export
 #' @examples
 #' hide("this is hidden")
 hide <- function(...) typst_function("hide", ...)
 
-#' @rdname typst_functions
+#' @rdname typst_function
 #' @export
 #' @examples
 #' pagebreak("this is hidden")
 pagebreak <- function(...) typst_function("pagebreak", ...)
 
-#' @rdname typst_functions
+#' @rdname typst_function
 #' @export
 #' @examples
 #' move(dx = pt(3), dy = pt(5), "pasta")
 move <- function(...) typst_function("move", ...)
 
-#' @rdname typst_functions
+#' @rdname typst_function
 #' @export
 #' @examples
 #' pad(x = percent(0) + pt(5), y = percent(10) + pt(0), image("file.svg"))
 pad <- function(...) typst_function("pad", ...)
 
-#' @rdname typst_functions
+#' @rdname typst_function
 #' @export
 #' @examples
 #' page(
@@ -91,13 +91,13 @@ pad <- function(...) typst_function("pad", ...)
 #')
 page <- function(...) typst_function("page", ...)
 
-#' @rdname typst_functions
+#' @rdname typst_function
 #' @export
 #' @examples
 #' scale(x = percent(-100), "This is mirrored")
 scale <- function(...) typst_function("scale", ...)
 
-#' @rdname typst_functions
+#' @rdname typst_function
 #' @export
 #' @examples
 #' skew(ax = deg(-12), "This is some fake italic text.")
